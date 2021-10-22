e2994874aa719a584f8fa6c325cb19bf3f705754
mklr-nl <info@mklr.nl>
2021-10-22 13:05:39 +0200

    wip

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,5 @@
 import * as React from "react"
-import Layout from '../components/Layout'
+import Layout from '../components/layout'
 import {createGlobalStyle} from 'styled-components'
 import img from '../assets/connected-dots.png'
 import styled from 'styled-components'
